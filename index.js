5c42f73fa391abcff2f16e670d23f8e1e4bb855f
Yash rana <97224327+YashRana03@users.noreply.github.com>
2023-09-06 12:07:27 +0100

    Add comments

--- index.js
+++ index.js
@@ -5,26 +5,29 @@ const appSettings = {
     databaseURL: "https://mobileapp-da427-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
+//Initialising the firebase database
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const groceryListInDB = ref(database, "groceryList")
 
+//Getting the html elements from document
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const groceryListEl = document.getElementById("grocery-list")
 const imgEl = document.querySelector("img");
 
-
+// Event that changes the cat image when mouse is on the img element
 imgEl.addEventListener("mouseover", function() {
 
     imgEl.src = "./cat-emoji2.gif"
 }) 
 
+// Event that changes the cat image when mouse leaves the img element
 imgEl.addEventListener("mouseleave", function() {
     imgEl.src = "./cat-emoji.gif"
 })
 
-
+// Event added to button so that text from input field gets pushed to the database
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     if(inputValue != ""  &  inputValue !=" ") {
@@ -34,10 +37,9 @@ addButtonEl.addEventListener("click", function() {
     }
     
     
-    
-    
 })
 
+// This function triggers whenever there is a change to database. It updates the grocery list by passing each item to the updateGroceryListEl()
 onValue(groceryListInDB, function(snapshot) {
 
     if(snapshot.exists()) {
@@ -58,10 +60,12 @@ onValue(groceryListInDB, function(snapshot) {
     
 })
 
+// Resets the input field
 function resetInputEl() {
     inputFieldEl.value = ""
 }
 
+// Creates a new li element for each items from the database. Attaches each li element an event listener so that it get deleted onclick. Adds the li element to the grocery list to be displayed
 function updateGroceryListEl(item) {
     
     let itemId = item[0]
@@ -81,6 +85,8 @@ function updateGroceryListEl(item) {
 
 }
 
+// Resets the grocery list element
 function resetGroceryListEl() {
     groceryListEl.innerHTML = ""
 }
+
